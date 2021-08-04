688c6f52e05cc3acc25fd4079f2e0142161494a1
emmanuelMartin53 <https://github.com/emmanuelMartin53/emmanuelMartin53.git>
2021-08-04 17:04:48 +0200

    systeme sugggestion

--- api/article/controllers/article.js
+++ api/article/controllers/article.js
@@ -13,4 +13,26 @@ module.exports = {
     const entity = await strapi.services.article.findOne({ slug });
     return sanitizeEntity(entity, { model: strapi.models.article });
   },
+  async related(ctx) {
+    const {slug} = ctx.params;
+    const article = await strapi.services.article.findOne({slug});
+
+    let articles = [];
+    article.categories.forEach(async (category) => {
+      const cat = await strapi.services.category.findOne({
+        _where: { name: category.name },
+      });
+      cat.articles.forEach((ar) => {
+        if (ar.id != article.id) {
+            articles.push(ar);
+        }
+      });
+    });
+    console.log(articles);
+
+    return sanitizeEntity(
+      { ...article, articles },
+      {model: strapi.models.article }
+    );
+  },
 };
